0acbe0984eac5622b08f598e7d3551549607d9f7
angelalvess <angeltrashangel@gmail.com>
2024-04-30 12:13:53 -0300

    feat: styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import Form from './components/Form/Form'
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello worldsssssssssssssss!
-    </h1>
+    <div className="bg-slate-900 min-h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="font-bold text-3xl text-white">Subscribe</h1>
+
+      <div className="w-96 mt-4 bg-stone-200 px-4 py-5 rounded-lg">
+        <Form />
+      </div>
+
+      <p className="text-slate-100 w-96 mt-2 text-center ">
+        Subscribe to our newsletter and get the latest news and updates.
+      </p>
+    </div>
   )
 }
 
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,59 @@
 const Form = () => {
-  return <div>Form</div>
+  return (
+    <form className="flex flex-col gap-2">
+      <div className="flex flex-col">
+        <label htmlFor="name">Name</label>
+        <input
+          className="px-2 py-1 rounded-md border border-gray-300 outline-none"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+        />
+      </div>
+
+      <div className=" flex flex-col  ">
+        <label htmlFor="email">Email</label>
+        <input
+          className="px-2 py-1 rounded-md border border-gray-300 outline-none"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+      </div>
+
+      <div className="flex justify-between mt-1">
+        <div className="flex gap-1">
+          <input
+            type="checkbox"
+            id="terms"
+            name="terms"
+            className="cursor-pointer"
+          />
+          <label
+            htmlFor="terms"
+            className="text-sm text-gray-600 cursor-pointer"
+          >
+            I agree to the terms
+          </label>
+        </div>
+        <a
+          className="text-blue-600 text-sm underline hover:text-blue-700"
+          href="#"
+        >
+          Read the terms
+        </a>
+      </div>
+
+      <button
+        type="submit"
+        className="bg-blue-600 text-white font-semibold mt-2 px-4 py-2 rounded-md hover:bg-blue-700"
+      >
+        Subscribe
+      </button>
+    </form>
+  )
 }
 
 export default Form
